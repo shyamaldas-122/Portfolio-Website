d627040fd242fa9af45e1030c7695d8a5e1469e4
Shyamal Das <shyamald934@gmail.com>
2024-01-19 15:40:38 +0530

    Resume Link Updated !

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/shyamaldas-122",
   resume:
-    "https://drive.google.com/file/d/1PKloMChDxA5LY1MVL6UPqOANxuJL7MCR/view?usp=drive_link",
+    "https://drive.google.com/file/d/1PKloMChDxA5LY1MVL6UPqOANxuJL7MCR/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/shyamal122/",
   twitter: "https://twitter.com/shyamal64118629",
   insta: "https://www.instagram.com/ig_shyamal_/",
